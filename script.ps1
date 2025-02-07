e42a4c63c3909b5f1a920d64ec88d035b3cda4cc
chrism9876 <chrism9876@hotmail.co.uk>
2025-02-07 13:40:53 +0000

    add commands

--- script.ps1
+++ script.ps1
@@ -1,1 +1,4 @@
-New-Item C:\test.txt -ItemType File
+$tag=($(curl.exe -s https://api.github.com/repos/mitmproxy/mitmproxy/releases/latest | select-string tag_name) -split "`"")[3]
+$version=$tag -replace "[a-zA-Z]",""
+
+curl.exe -O  "https://downloads.mitmproxy.org/$version/mitmproxy-$version-windows-x86_64-installer.exe"
